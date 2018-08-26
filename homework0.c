f9c4ed4f138b110dd8cc39a4c231504e074ac0a0
NicolasDelacruz <nicolasd@iastate.edu>
2018-08-26 09:29:09 -0500

    updating homework 0

---
+++ homework0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//The variable sizeOfBoard will determine size of board (sizeOfBoard x sizeOfBoard)
+int sizeOfBoard = 5;
+
+
+int main(int argc, char const *argv[]){
+  //TO DO
+  
+  return 0;
+}
