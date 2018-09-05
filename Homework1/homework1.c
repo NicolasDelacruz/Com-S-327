cb038a4c800866c09612cdf183cc91c3823453bb
NicolasDelacruz <nicolasd@iastate.edu>
2018-09-04 19:23:13 -0500

    Fully functioning program that concludes assignment 1.

--- Homework1/homework1.c
+++ Homework1/homework1.c
@@ -11,12 +11,23 @@
 
 int terminalHeight = 24;
 
+int numberOfRooms = 5;
+
 struct tile {
   int hardness;
   char value;
 };
 
+struct room {
+  int x;
+  int y;
+  int width;
+  int height;
+};
+
 struct tile map[mapHeight][mapWidth];
+
+struct room rooms[5];
 
 
 /*
@@ -58,11 +69,15 @@ void generateMap(){
   }
 }
 
-void placeRoom(int givenx, int giveny, int givenWidth, int givenHeight){
+void placeRoom(int givenx, int giveny, int givenWidth, int givenHeight, int roomIndex){
   int i,j;
 
   for(i = giveny; i <= (giveny + givenHeight); ++i){
     for(j = givenx; j <= (givenx + givenWidth); ++j){
+      rooms[roomIndex].x = givenx;
+      rooms[roomIndex].y = giveny;
+      rooms[roomIndex].width = givenWidth;
+      rooms[roomIndex].height = givenHeight;
       map[i][j].hardness = 0;
       map[i][j].value = '.';
     }
@@ -70,23 +85,69 @@ void placeRoom(int givenx, int giveny, int givenWidth, int givenHeight){
 
 }
 
+void sortAllRooms(){
+  int i, j, minIndex;
+  for(i = 0; i < 4; ++i){
+    minIndex = i;
+    for(j = i+1; j < 5; ++j){
+      if(rooms[j].x < rooms[minIndex].x){
+	struct room tempRoom = rooms[i];
+	rooms[i] = rooms[j];
+	rooms[j] = tempRoom;
+      }
+    }
+  }
+}
+
+void addCorridors(int givenx1, int giveny1, int givenx2, int giveny2){
+  int i, j, increment;
+
+  if(giveny2 - giveny1 != 0){
+    increment =  abs(giveny2 - giveny1)/(giveny2-giveny1);
+  }
+
+  for(i = giveny1; i !=  giveny2; i += increment){
+    if(map[i][givenx1].value == ' '){
+      map[i][givenx1].value = '#';
+    }
+  }
+
+  if(givenx2 - givenx1 != 0){
+    increment =  abs(givenx2 - givenx1)/(givenx2-givenx1);
+  }
+  for(j = givenx1; j != givenx2; j += increment){
+    if(map[giveny2][j].value == ' '){
+      map[giveny2][j].value = '#';
+    }
+  }
+}
+
+void placeCorridors(){
+  sortAllRooms();
+  int i, x1, x2, y1, y2;
+
+  
+  for(i = 0; i < 4; ++i){
+    x1 = rooms[i].x;
+    y1 = rooms[i].y;
+    x2 = rooms[i+1].x;
+    y2 = rooms[i+1].y;
+    addCorridors(x1,y1,x2,y2);
+  }
+
+  x1 = rooms[4].x;
+  y1 = rooms[4].y;
+  x2 = rooms[0].x;
+  y2 = rooms[0].y;
+  addCorridors(x1,y1,x2,y2);
+}
+
 
 bool validRoom(int givenX, int givenY, int givenWidth, int givenHeight){
   int i, j;
 
-  int tempX = givenX;
-  int tempY = givenY;
-
-  if(givenY > 1){
-    tempY -= 1;
-  }
-
-  if(givenX > 1){
-    tempX -= 1;
-  }
-
-  for(i = tempY; i <= (tempY + givenHeight +1); ++i){
-    for(j = tempX; j <= (tempX + givenWidth +1); ++j){
+  for(i = (givenY-1); i <= (givenY + givenHeight +1); ++i){
+    for(j = (givenX-1); j <= (givenX + givenWidth +1); ++j){
       if(map[i][j].value != ' '){
 	return false;
       }
@@ -105,7 +166,7 @@ void generateAllRooms(){
     x = generateRandomNum(1,(mapWidth - doorWidth));
     y = generateRandomNum(1,(mapHeight - doorHeight));
     if(validRoom(x,y,doorWidth,doorHeight)){
-      placeRoom(x, y, doorWidth, doorHeight);
+      placeRoom(x, y, doorWidth, doorHeight, i);
     }
     else{
       i--;
@@ -151,6 +212,8 @@ int main(int argc, char const *arfv[]){
   generateMap();
 
   generateAllRooms();
+
+  placeCorridors();
 
   printMap();
 }
