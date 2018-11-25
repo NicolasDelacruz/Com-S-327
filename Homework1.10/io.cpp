13896b779b857228e50e79175347b7e963fa0896
NicolasDelacruz <nicolasd@iastate.edu>
2018-11-25 16:36:16 -0600

    Added placing the pieces to the board.

--- Homework1.10/io.cpp
+++ Homework1.10/io.cpp
@@ -7,6 +7,33 @@
 #include "grid.h"
 
 
+static int get_lowest_location(grid *g, int x)
+{
+  int y;
+  for(y = GRID_HEIGHT-1; y > -1; --y){
+    if(g->map[y][x] == 0){
+      return y;
+    }
+  }
+  return -1;
+}
+
+static int place_move(grid *g, int x){
+  int y;
+
+  y = get_lowest_location(g, x);
+  if(y == -1){
+    return 0;
+  }
+  if(g->turn == 1){
+    g->map[y][x] = '1';
+  }else{
+    g->map[y][x] = '2';
+  }
+  return 1;
+}
+
+
 static void place_border(void)
 {
   int i, j, width, height;
@@ -71,16 +98,15 @@ void io_display(grid *g)
   for(i = 0; i < GRID_HEIGHT; ++i){
     for(j = 0; j < GRID_WIDTH; ++j){
       switch(g->map[i][j]){
-      case 'O':
-	attron(COLOR_PAIR(g->color_map[i][j]));
+      case '1':
+	attron(COLOR_PAIR(1));
 	mvaddch(i+2, j+2, 'O');
-	attroff(COLOR_PAIR(g->color_map[i][j]));
-	break;      
-      default:
-	attron(COLOR_PAIR(6));
-        mvaddch(i+2, j+2, 'X');
-	attroff(COLOR_PAIR(6));
+	attroff(COLOR_PAIR(1));
 	break;
+      case '2':
+	attron(COLOR_PAIR(2));
+	mvaddch(i+2, j+2, 'O');
+	attroff(COLOR_PAIR(2));
       }
     }
     putchar('\n');
@@ -132,21 +158,28 @@ void io_handle_input(grid *g)
       break;
     case 's':
     case '5':
+      //add new thing
+      //if place_move failed then end case
+      if(!place_move(g, location-2)){
+	break;
+      }
+
+      //display new map
+      io_reset_terminal();
+      io_display(g);
+      attron(COLOR_PAIR(g->turn));
+      mvaddch(0, location, 'O');
+      attroff(COLOR_PAIR(g->turn));
+
+      //switch turns
       if(g->turn == 1){
 	g->turn = 2;
       }
       else if(g->turn == 2){
 	g->turn = 1;
       }
-
-
-      io_reset_terminal();
-      io_display(g);
-      attron(COLOR_PAIR(g->turn));
-      mvaddch(0, location, 'O');
-      attroff(COLOR_PAIR(g->turn));
       break;
-    case 'q':
+    case 't':
       g->game_over = 1;
       break;
     }
